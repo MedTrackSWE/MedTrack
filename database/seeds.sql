39b2a20bbbd4e1326841a8c1f8e4f31e83818ba0
Luke Saleh <lukesaleh@ufl.edu>
2024-10-14 22:22:48 -0400

    Wrote example seeds

---
+++ database/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO Users (username, password_hash, email) VALUES 
+('user1', '$2b$12$hashedPasswordHere', 'user1@example.com'),
+('user2', '$2b$12$hashedPasswordHere', 'user2@example.com');
