e8c6c50cb6083b145f231f8041ad6025582a29cd
Luke Saleh <lukesaleh@ufl.edu>
2024-11-07 20:00:12 -0500

    Draft of medical history view

---
+++ backend/app/views/medical_history.py
@@ -0,0 +1,21 @@
+# views.py
+
+from flask import Blueprint, jsonify, request
+from models.medical import MedicalRecord
+
+medical_history_bp = Blueprint('medical_history', __name__)
+
+@medical_history_bp.route('/api/medical_history/appointments', methods=['GET'])
+def get_appointments():
+    user_id = get_jwt_identity()
+    medical_record = MedicalRecord(user_id)
+    appointments = medical_record.get_prior_appointments()
+    return jsonify(appointments), 200
+
+@medical_history_bp.route('/api/medical_history/conditions', methods=['GET'])
+@jwt_required()
+def get_conditions():
+    user_id = get_jwt_identity()
+    medical_record = MedicalRecord(user_id)
+    conditions = medical_record.get_prior_conditions()
+    return jsonify(conditions), 200
