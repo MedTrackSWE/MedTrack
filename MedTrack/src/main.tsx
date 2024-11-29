2eff657e67637305f5b33beee2bc9ad797e12bf6
emmaherrero <emmaherrero@ufl.edu>
2024-11-29 12:35:20 -0500

    Appointment Scheduler
    Current version of AppointmentScheduler.tsx. Still figuring out how to fix routing issues

--- MedTrack/src/main.tsx
+++ MedTrack/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import AppointmentScheduler from './pages/AppointmentScheduler';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/appointment-scheduler" element={<AppointmentScheduler />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
